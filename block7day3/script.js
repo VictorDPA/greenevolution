0f76faf23a8ca919706eaaeceb024d309a199d99
VictorDPA <vicnoob@gmail.com>
2022-09-25 22:15:53 -0300

    do all

--- block7day3/script.js
+++ block7day3/script.js
@@ -1,3 +1,9 @@
 const sum = (a, b) => a + b;
 const div = (a, b) => a / b;
 module.exports = sum;
+
+function summationOf(num) {
+  if (num === 1) return num;
+  return num + summationOf(num - 1);
+}
+
